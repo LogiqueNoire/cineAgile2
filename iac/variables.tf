d4bc648a35b545191949fb66f5565c6882d341f2
logiquenoire <leandromauricci@gmail.com>
2025-09-30 17:52:01 -0500

    fix: comentar variables sin uso

--- iac/variables.tf
+++ iac/variables.tf
@@ -2,7 +2,7 @@ variable "region" {
     type = string
     description = "Region of AWS"
 }
-
+/* Variables para cuando haya dominio
 variable "domain_name" {
   description = "El nombre de tu dominio principal (ej. midominio.com)."
   type        = string
@@ -12,3 +12,4 @@ variable "route53_zone_id" {
   description = "El ID de la Hosted Zone de Route 53 para tu dominio."
   type        = string
 }
+*/
