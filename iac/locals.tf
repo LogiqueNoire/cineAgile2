4299d6dbdc4adfaf679e7c65e0c714a20eddca23
logiquenoire <leandromauricci@gmail.com>
2025-10-01 01:08:11 -0500

    fix: usar nombres de clusters adecuados, corregir jenkiisfile.frontend

--- iac/locals.tf
+++ iac/locals.tf
@@ -1,11 +1,6 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "cineagile-eks-${random_integer.suffix.result}"
-  cluster_name_2 = "cineagile-eks-${random_integer.suffix.result}"
-}
-
-resource "random_integer" "suffix" {
-  min = 100
-  max = 999
+  cluster_name = "cineagile-eks-942"
+  cluster_name_2 = "cineagile-eks-111"
 }
