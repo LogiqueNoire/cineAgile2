5b3a9986748fffbdc68363ac9fb21f8eed8197ab
logiquenoire <leandromauricci@gmail.com>
2025-10-26 23:55:49 -0500

    feat: :dominio gestionado por route53

--- iac/frontend.tf
+++ iac/frontend.tf
@@ -1,8 +1,108 @@
-// --- 1. S3 Bucket para el Contenido Estático ---
+// 0. Zona route 53
+resource "aws_route53_zone" "main" {
+  name = "cineagile.com"
+}
 
+# Certificado SSL para CloudFront
+resource "aws_acm_certificate" "cineagile_cert" {
+  provider          = aws.use1
+  domain_name       = "cineagile.com"
+  validation_method = "DNS"
+
+  subject_alternative_names = ["www.cineagile.com"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Validación DNS automática usando Route 53
+resource "aws_route53_record" "cineagile_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cineagile_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "cineagile_cert_validation" {
+  provider                = aws.use1
+  certificate_arn         = aws_acm_certificate.cineagile_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cineagile_cert_validation : record.fqdn]
+
+  depends_on = [aws_route53_record.cineagile_cert_validation]
+}
+
+
+# Ruta raiz
+resource "aws_route53_record" "root_domain" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "cineagile.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  depends_on = [aws_cloudfront_distribution.s3_distribution]
+}
+
+# Subdominio
+resource "aws_route53_record" "www_domain" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.cineagile.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  depends_on = [aws_cloudfront_distribution.s3_distribution]
+}
+
+// 1. S3 Bucket para el Contenido Estático
+
+#checkov:skip=CKV2_AWS_62:Bucket se usa solo para el hosting est
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = "cineagile-front"
+
 }
+
+# solucionado CKV_AWS_18:Ensure the S3 bucket has access logging enabled, Configuración de logging
+resource "aws_s3_bucket" "frontend_logs" {
+  bucket = "cineagile-front-logs"
+}
+
+resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  target_bucket = aws_s3_bucket.frontend_logs.id
+  target_prefix = "log/"
+}
+
+
+#solucionado CKV_AWS_145 Ensure that S3 buckets are encrypted with KMS by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "frontend_encryption" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
   bucket = aws_s3_bucket.frontend_bucket.id
@@ -60,21 +160,31 @@ resource "aws_s3_bucket_policy" "s3_access_policy" {
 
 // --- 4. CloudFront Distribution (CDN) ---
 
+# checkov:skip=CKV2_AWS_42:No se está usando un dominio
 resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  depends_on = [aws_acm_certificate_validation.cineagile_cert_validation]
+
+  aliases = [
+    "cineagile.com",
+    "www.cineagile.com"
+  ]
+
+  # --- ORIGIN 1: S3 ---
   origin {
     domain_name              = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
     origin_id                = aws_s3_bucket.frontend_bucket.id
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
+  # Comportamiento por defecto: sirve S3
   default_cache_behavior {
+    target_origin_id       = aws_s3_bucket.frontend_bucket.id
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = aws_s3_bucket.frontend_bucket.id
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
@@ -88,23 +198,51 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
   }
 
+  # Comportamiento para API (rutas que van a ALB)
+  # ordered_cache_behavior {
+  #   path_pattern           = "/api/*"
+  #   target_origin_id       = "alb-backend"
+  #   viewer_protocol_policy = "redirect-to-https"
+  #   allowed_methods        = ["GET", "HEAD", "POST", "PUT", "DELETE"]
+  #   cached_methods         = ["GET", "HEAD"]
+
+  #   forwarded_values {
+  #     query_string = true
+  #     cookies {
+  #       forward = "all"
+  #     }
+  #   }
+  # }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
 
-  // Usa certificado default de CloudFront (sin ACM)
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn      = aws_acm_certificate.cineagile_cert.arn
+    ssl_support_method        = "sni-only"
+    minimum_protocol_version  = "TLSv1.2_2021"
   }
 
-  // Sin aliases porque no hay dominio propio
-  aliases = []
-
-  custom_error_response {
-    error_code         = 403
-    response_code      = 200
-    response_page_path = "/index.html"
-  }
 }
+
+# #Health checks
+# resource "aws_route53_health_check" "alb_east_1" {
+#   fqdn              = aws_lb.alb_east_1.dns_name
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/health"
+#   failure_threshold = 3
+#   request_interval  = 30
+# }
+
+# resource "aws_route53_health_check" "alb_east_2" {
+#   fqdn              = aws_lb.alb_east_2.dns_name
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/health"
+#   failure_threshold = 3
+#   request_interval  = 30
+# }
--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,9 +1,17 @@
-/*
-output "frontend_url" {
-  description = "El URL del sitio web con HTTPS."
-  value       = "https://${var.domain_name}"
+output "route53_zone_id" {
+  description = "ID de la zona hospedada en Route 53"
+  value       = aws_route53_zone.main.zone_id
 }
-*/
+
+output "route53_zone_name" {
+  description = "Nombre del dominio en Route 53"
+  value       = aws_route53_zone.main.name
+}
+
+output "route53_name_servers" {
+  description = "Servidores de nombre asignados por AWS"
+  value       = aws_route53_zone.main.name_servers
+}
 
 output "s3_frontend_bucket_name" {
   description = "El nombre del bucket de S3 para el CI/CD del frontend (donde se suben los archivos)."
