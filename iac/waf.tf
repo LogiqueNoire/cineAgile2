5f1992ffa245a91a9e433f7bff34025a4b328e6a
calingod <joguisaz454@outlook.com>
2025-11-29 03:14:11 -0500

    waf clasico

--- iac/waf.tf
+++ iac/waf.tf
@@ -1,15 +1,57 @@
-//Falta definir los predicate en las métricas
-resource "aws_wafregional_rule" "waf_allowed" { //permitir solo trafico por hacer
+//Waf clasico provisional
+
+#que paises limitaremos?
+resource "aws_waf_geo_match_set" "geo_test" {
+  name = "geo_test"
+
+  geo_match_constraint {
+    type = "COUNTRY"
+    value = "PE"
+  }
+
+  geo_match_constraint {
+    type = "COUNTRY"
+    value = "US"
+  }
+}
+
+resource "aws_wafregional_rule" "waf_allowed" { 
   name        = "waf_allowed"
   metric_name = "AllowedRequests" 
+
+  predicate {
+    data_id = aws_waf_geo_match_set.geo_test.id
+    negated = false
+    type = "GeoMatch"
+  }
 }
 
-resource "aws_wafregional_rule" "waf_blocked" { //sql injection por hacer
-  name        = "waf_blocked"
-  metric_name = "BlockedRequests" 
+
+resource "aws_wafregional_sql_injection_match_set" "sql" {
+  name = "sql_injection"
+
+  sql_injection_match_tuple {
+    text_transformation = "URL_DECODE"
+
+    field_to_match {
+      type = "QUERY_STRING"
+    }
+  }
 }
 
-resource "aws_wafregional_rule" "waf_count" {   // Contar tráfico con un header sospechoso por hacer
+resource "aws_wafregional_rule" "waf_block_sql" {  
+  name        = "waf_block_sql"
+  metric_name = "BlockedSqlI" 
+
+  predicate {
+    data_id = aws_wafregional_sql_injection_match_set.sql.id
+    negated = false
+    type = "SqlInjectionMatch"
+  }
+}
+
+#falta predicate
+resource "aws_wafregional_rule" "waf_count" {   
   name        = "waf_count"
   metric_name = "CountRequests" 
 }
@@ -34,7 +76,7 @@ resource "aws_wafregional_rule_group" "waf_group" {
     }
 
     priority = 30
-    rule_id = aws_wafregional_rule.waf_blocked.id
+    rule_id = aws_wafregional_rule.waf_block_sql.id
   }  
 
     activated_rule {
@@ -50,10 +92,10 @@ resource "aws_wafregional_rule_group" "waf_group" {
 
 resource "aws_wafregional_web_acl" "waf_acl" {
   name        = "waf_acl"
-  metric_name = "example"
+  metric_name = "wafacl"
 
   default_action {
-    type = "ALLOW"
+    type = "BLOCK"
   }
 
   rule {
@@ -67,8 +109,13 @@ resource "aws_wafregional_web_acl" "waf_acl" {
   }
 }
 
-//Falta crear el web acl
-resource "aws_wafregional_web_acl_association" "foo" {
-  resource_arn = aws_lb.lb_good_1.arn
-  web_acl_id = aws_wafregional_web_acl.foo.id
+
+resource "aws_wafregional_web_acl_association" "wacl1" {
+  resource_arn = aws_lb.alb_us_east_1.arn
+  web_acl_id = aws_wafregional_web_acl.waf_acl.id
+}
+
+resource "aws_wafregional_web_acl_association" "wacl2" {
+  resource_arn = aws_lb.alb_us_east_2.arn
+  web_acl_id = aws_wafregional_web_acl.waf_acl.id
 }
