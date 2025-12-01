cd0a49342b14a429bda383c58d5fdad90a1ad8c3
calingod <joguisaz454@outlook.com>
2025-12-01 17:18:08 -0500

    feat: agregar modulo de los bukcet logs

--- iac/modules/bucket_logs_s3/output.tf
+++ iac/modules/bucket_logs_s3/output.tf
@@ -1,17 +1,7 @@
-# Output variable definitions
-
-output "bucket"{
-    value = aws_s3_bucket.this.bucket
+output "bucket_id" {
+  value = aws_s3_bucket.this.id
 }
 
 output "bucket_arn" {
   value = aws_s3_bucket.this.arn
-}
-
-output "bucket_name"{
-    value = aws_s3_bucket.this.bucket_domain_name
-}
-
-output "bucket_id"{
-    value = aws_s3_bucket.this.id
 }
