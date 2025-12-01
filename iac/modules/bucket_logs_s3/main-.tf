9b6e6ebdc917da8b8dcf78097a18dbe259a678bd
calingod <joguisaz454@outlook.com>
2025-12-01 17:18:08 -0500

    feat: agregar modulo de los bukcet logs

--- iac/modules/bucket_logs_s3/main-.tf
+++ iac/modules/bucket_logs_s3/main-.tf
@@ -1,73 +1,45 @@
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
   force_destroy = true
-  tags = var.tags
+  tags          = var.tags
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  region = var.region
   bucket = aws_s3_bucket.this.id
+
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_logging" "this" {
-  region = var.region
-  bucket = aws_s3_bucket.this.id
-
-  target_bucket = var.log_bucket_acces
-  target_prefix = "/bucket_logs_s3/${var.bucket_name}"
-}
-
-
-resource "aws_s3_bucket_lifecycle_configuration" "this" {
-  region = var.region
-  bucket = aws_s3_bucket.this.id
-
-    rule {
-    id      = "expire"
-    status  = "Enabled"
-
-    filter {
-      
-    }
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-    expiration {
-      days = 90
-    }
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "this" {
-  region = var.region
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  region = var.region
-  bucket = aws_s3_bucket.this.bucket
+  bucket = aws_s3_bucket.this.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:Kms"
+      sse_algorithm = "AES256"
     }
   }
 }
 
-resource "aws_s3_bucket_notification" "this" {
-  region = var.region
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
   bucket = aws_s3_bucket.this.id
-    topic {
-    topic_arn     = aws_sns_topic.bucket_notifications.arn
-    events        = ["s3:ObjectCreated:*"]
-    filter_prefix = "logs/"
+
+  rule {
+    id     = "delete-old"
+    status = "Enabled"
+
+    expiration {
+      days = 90
+    }
   }
 }
--- iac/modules/bucket_logs_s3/variables.tf
+++ iac/modules/bucket_logs_s3/variables.tf
@@ -1,22 +1,8 @@
-
 variable "bucket_name" {
-  description = "Name of the s3 bucket. Must be unique."
-  type        = string
-}
-
-variable "tags" {
-  description = "Tags to set on the bucket."
-  type        = map(string)
-  default     = {}
-}
-
-
-
-variable "region" {
   type = string
 }
 
-variable "log_bucket_acces" {
-  nullable = true
-  default = null
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
